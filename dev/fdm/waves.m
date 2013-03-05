c0f7663d331656409b87111345f611fca4bc2dfa
Anders Gidenstam <anders@gidenstam.org>
2013-03-05 22:09:02 +0100

    Added some more octave wave simulation.

--- dev/fdm/waves.m
+++ dev/fdm/waves.m
@@ -3,83 +3,91 @@
 %%  This file is licensed under the GPL license version 2 or later.
 %%
 
-FT  = 3.2808399;
+%% Formulas
+%% ([1] ICMM_TGZielinski_FluidWaves.slides.pdf,
+%%  [2] http://http.developer.nvidia.com/GPUGems/gpugems_ch01.html,
+%%  [3] http://en.wikipedia.org/wiki/Airy_wave_theory and so on)
+% Wave
+%  length              L     [m]
+%  amplitude           A     [m]
+%  angular frequency   omega [rad/s]
+%  frequency           f     [1/s]
+%  wave number         k     [rad/m]
+%  phase velocity      c     [m/s]
+%  phase speed         S     [m/s]
+%  wave number         k     [rad/m]?
+%  period              T     [s]
+%  direction           D     unit vector
+%
+%  wave(x,y,t)     = A*cos(dot(D,(x,y))*k - omega*t)
+%  dwave(x,y,t)/dx = 
+%  dwave(x,y,t)/dy = 
+%
+%  omega = 2*pi / T = 2*pi * f
+%  k = 2*pi / L
+%  c = omega / k = L / T = L * f
+%
+%  In deep water [1]: (Check c!)
+%    omega = sqrt(g*k)
+%    c = sqrt(g * k)
+%
+%  In finite depth (depth h) [1]:
+%    omega = sqrt(g * k * tanh(k*h))
+%    c = sqrt(g/k * tanh(k*h))
+%
+%  In shallow water (depth h) [1]:
+%    omega = sqrt(g*h) * k
+%    c = sqrt(g*h) (also the group velocity here!)
+
+M   = 3.2808399;
 g   = 32.185039;
 KTS = 1.6878099;
+DEG = pi/180;
 
-HRPx = 12.0*FT;
-HRPy =  0.0*FT;
+# The hull
+HRPx = 12.0*M;
+HRPy =  0.0*M;
 HRPagl = 5.6;
 
 Hull = \
  [0.00  0.24  0.48  0.75  1.20  2.00  3.70  6.17  8.29  8.30  11.70 11.74 20.00 20.00;
-   0.00 -0.44 -0.87 -1.46 -1.80 -1.96 -2.09 -2.18 -2.20 -2.11 -2.13 -1.98 -1.98 -0.25].*FT;
+   0.00 -0.44 -0.87 -1.46 -1.80 -1.96 -2.09 -2.18 -2.20 -2.11 -2.13 -1.98 -1.98 -0.25].*M;
 
+LWL  = 20 * M; # Load water line (approx) [ft]
+BEAM =  5 * M; # Water line beam (approx) [ft]
 
-LWL = 20 * FT; # Load water line (approx) [ft]
-V = 1:1:50;           # Velocity [kts]
+course = 0 * DEG; # [rad]
 
+# Sinus shaped swell
+Aw = 1.0 * M; # [ft]
+Lw = 10.0 * M; # [ft]
 
-SLR = V./sqrt(LWL);
+# direction
+Dw = 30 * DEG; #[0:5:360] * DEG; # [rad]
 
-# First order approx. of self generated transverse wave V=Ck=1.3*sqr(Lw)
-# More formally phase speed c = sqrt(g/(2*pi)*l) where l is the wave length
-# Note: The divergent waves of the wake contribute little to the resistance.
-#Lw = (V./1.3).^2;      # Wave length [ft]
+# Wave speed [ft/s]
+Vw = sqrt(g * 2*pi / Lw);
 
-Lw = 2*pi/g * (V.*KTS).^2;
+# Wave period [s]
+Tw = Lw./Vw;
 
-# Amplitude (no guess at what it is yet)
-# Perhaps http://www.bellona.org/reports/1193670187.13 might have something.
-# Total energy per unit lenght:
-#   E = 1/8 * c*g*h^2
-# Flux of energy through a vertical plane strip of width 1 perpendicular
-# to the wave:
-#   F = 1/32 * c*g*h^2*T
-#  where T is the wave period (T = 1/f = Lw/v),
-#        c is the water density,
-#        g is the gravity,
-#        h is the wave amplitude.
-#
-# Assumption: Energy per second is P=drag*v
-# => Aw = h = sqrt(8 * drag * v / (c*g))
-# Note: The units do not add up!
+# Effective wave speeds and lengths
+loVw = Vw.*cos(Dw);
+trVw = Vw.*sin(Dw);
+loLw = Lw./cos(Dw);
+trLw = Lw./sin(Dw);
 
-# New take from
-#  http://farside.ph.utexas.edu/teaching/336l/fluidhtml/node125.html
-#
-# In deep water:
-#   D = 1/4 * \rho * g * a^2 * \omega, \omega = 2*pi / T = 2*pi * v/Lw
-# gives
-#   a = sqrt(2 * D * Lw / (\rho * g * pi * v))
+x = [0:0.5:100] * M;
 
-Aw = 1;
+wave = Aw * cos(2*pi./Lw .* x);
 
-x = 0:0.2:LWL;
+plot(x, Aw * cos(2*pi./Lw .* x),\
+     x, Aw * cos(2*pi./Lw .* x),\
+     x, Aw * cos(2*pi./Lw .* x));
 
-# Wave height at a position x: Hw = Aw * cos(2*pi/Lw * x);
-Hw = [Aw .* cos(2*pi./Lw .* x(1))' Aw .* cos(2*pi./Lw .* x(length(x)))'];
+#plot(x, wave,
+#     Hull(1,:), Hull(2,:)+HRPagl);
 
-#plot(V,Lw*0.3048);
+t = 0:0.1:60;
+#plot(t, Aw * cos(2*pi.*Vw/Lw .*t));
 
-# Assume the effective water line angle is defined by the line from
-# the water line at bow and stern. Not a very good one.
-deltaTrim = -atan2(Hw(:,1)-Hw(:,2), LWL);
-
-# Mean water line height - could be used for squat.
-# integrate Aw .* cos(2*pi./Lw(v) .* x) from 0 to LWL
-deltaWL = Aw .* Lw./(2*pi) .* sin(2*pi./Lw .* LWL)./LWL;
-
-# Looking at one particular speed:
-v=12;
-Vkt = V(v)
-SLR = SLR(v)
-dTrim = deltaTrim(v)
-
-deltaWLlin = interp1([0 LWL], Hw(v,:), [HRPx], 'linear')
-
-deltaWLint = deltaWL(v)
-
-plot(x, Aw .* cos(2*pi./Lw(v) .* x),\
-     [x(1) x(length(x))], Hw(v,:),\
-     Hull(1,:), Hull(2,:)+HRPagl+0*deltaWLint);
